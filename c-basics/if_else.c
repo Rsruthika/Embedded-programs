6eae76abd6b5c755a369ec35ba9be17a6ad73197
Rsruthika <116784380+Rsruthika@users.noreply.github.com>
2026-01-06 10:15:29 +0530

    LED blink simulation

--- c-basics/if_else.c
+++ c-basics/if_else.c
@@ -1,1 +1,19 @@
 
+#include <stdio.h>
+#include <unistd.h> // For sleep function (simulate delay)
+
+int main() {
+    int led_state = 0; // 0 = OFF, 1 = ON
+
+    while(1) {
+        led_state = !led_state; // Toggle LED
+        if(led_state) {
+            printf("LED is ON\n");
+        } else {
+            printf("LED is OFF\n");
+        }
+        sleep(1); // Delay 1 second
+    }
+
+    return 0;
+}
